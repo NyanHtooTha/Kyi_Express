ae8bf951d3a5d89811cdd26dc4d0d051bf038acd
Nyan Htoo Tha <nyanhtootha@gmail.com>
2019-03-11 23:07:05 +0630

    fix indent

--- awb/serializers.py
+++ awb/serializers.py
@@ -15,16 +15,16 @@ class AWBSerializer(serializers.HyperlinkedModelSerializer):
         
 
     def create(self, validated_data):
-            sender_data = validated_data['sender']
-            receiver_data = validated_data['receiver']
-            if sender_data:
-                sender_serializer = Kyi_Express_User_Serializer(data=sender_data)
-                if sender_serializer.is_valid():
-                    sender_id = sender_serializer.save()
-                validated_data['sender'] = sender_id
-            if receiver_data:
-                receiver_serializer = Kyi_Express_User_Serializer(data=receiver_data)
-                if receiver_serializer.is_valid():
-                    receiver_id = receiver_serializer.save()
-                validated_data['receiver'] = receiver_id
-            return AWB.objects.create(**validated_data)
+        sender_data = validated_data['sender']
+        receiver_data = validated_data['receiver']
+        if sender_data:
+            sender_serializer = Kyi_Express_User_Serializer(data=sender_data)
+            if sender_serializer.is_valid():
+                sender_id = sender_serializer.save()
+            validated_data['sender'] = sender_id
+        if receiver_data:
+            receiver_serializer = Kyi_Express_User_Serializer(data=receiver_data)
+            if receiver_serializer.is_valid():
+                receiver_id = receiver_serializer.save()
+            validated_data['receiver'] = receiver_id
+        return AWB.objects.create(**validated_data)
