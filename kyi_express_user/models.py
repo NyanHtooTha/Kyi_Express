2e1b76589fc00d41c48f3393644a40de52b79714
Nyan Htoo Tha <nyanhtootha@gmail.com>
2019-03-11 17:26:07 +0630

    edit wrong typo on models

--- kyi_express_user/models.py
+++ kyi_express_user/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Kyi_Express_User(models.Model):
-    name = models.CharField(max_length=50, blank=False, default="Sender")
-    mobile = models.CharField(max_length=25, blank=False, default="Receiver")
+    name = models.CharField(max_length=50, blank=False)
+    mobile = models.CharField(max_length=25, blank=False)
     address = models.TextField()
     
